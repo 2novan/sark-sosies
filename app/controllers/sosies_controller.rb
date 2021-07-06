dfec7e176c1f4248fd28c58d293d70d5877db216
Denovan Niget <niget.denovan@gmail.com>
2021-07-06 19:50:50 +0200

    SosiesController create new

--- app/controllers/sosies_controller.rb
+++ app/controllers/sosies_controller.rb
@@ -1,2 +1,20 @@
 class SosiesController < ApplicationController
+  def new
+    @sosie = Sosie.new
+  end
+
+  def create 
+    @sosie = Sosie.new(sosie_params)
+    if @sosie.save
+      redirect_to sosie_path(@sosie)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def sosie_params
+    params.require(:sosie).permit(:name, :description, :address, :price_per_day )
+  end
 end
