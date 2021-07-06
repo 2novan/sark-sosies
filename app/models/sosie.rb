80dcedb7e19c8e26ab45c949ff61645f707d5ad1
norman bagne <norman.bagne@gmail.com>
2021-07-06 20:08:27 +0200

    added cloudinary + active storage support for User model and Sosie model

--- app/models/sosie.rb
+++ app/models/sosie.rb
@@ -2,6 +2,6 @@ class Sosie < ApplicationRecord
   belongs_to :user
   validates :name, :description, :address, :price_per_day, presence: true
   has_many :bookings
+  has_one_attached :photo
   end
-  
 end
