eed0399d3ee998542aec805948338d39b2511f6e
Denovan Niget <niget.denovan@gmail.com>
2021-07-15 19:14:09 +0200

    better routes + dates

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # resources :users
   get "/profile", to: "users#show", as: "profile"
-  get "/accept/:id", to: "bookings#accept", as: "accept"
-  get "/refuse/:id", to: "bookings#refuse", as: "refuse"
-  get "/delete/:id", to: "bookings#destroy", as: "destroy"
-
-
-  
   resources :sosies do
     resources :bookings, only: %i[new create show]
+  end
+  resources :bookings, only: [:destroy] do
+    patch :accept, on: :member
+    patch :refuse, on: :member
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end 
